d497894d52995fa9f1a32e13615914cef62281f8
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-08-17 19:14:24 -0700

    Task 2.2
    Implement http, url, and fs modules in Node.js

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
-const http = require("http");
+const http = require("http"),
+    url = require("url"),
+    fs = require("fs");
 
 http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Node!\n");
+    let addr = request.url,
+     q =  url.parse(addr, true),
+     filePath = "";
+
+    if (q.pathname.includes("documentation")) {
+        filepath = (__dirname + "/documentation.html");
+     } else {
+        filepath = "index.html";
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(data);
+        response.end;
+    })
+
+    fs.appendFile("log.txt", "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n", (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Added to log.");
+        }
+    });
+
 }).listen(8080);
 
 console.log("My first Node test server is running on port 8080.");
