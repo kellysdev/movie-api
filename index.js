c6110e6233b2adf31fd65b52057dbd2dcf916f53
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-08-21 19:13:16 -0700

    Task 2.4 Step 2

--- index.js
+++ index.js
@@ -1,2 +1,83 @@
-const express = require("express");
+//import
+const express = require("express"),
+  url = require("url"),
+  morgan = require("morgan");
+
+//declare variables
 const app = express();
+
+let topMovies = [
+  {
+	title: "Howl's Moving Castle",
+	genre: "Romance"
+  },
+  {
+	title: "Princess Mononoke",
+	genre: "Action"
+  },
+  {
+    title: "Spirited Away",
+    genre: "Coming of Age"
+  },
+  {
+    title: "The Secret World of Arriety",
+    genre: "Adventure"
+  },
+  {
+    title: "Castle in the Sky",
+    genre: "Fantasy"
+  },
+  {
+    title: "Nausicaa of the Valley of the Wind",
+    genre: "Fantasy"
+  },
+  {
+    title: "Grave of the Fireflies",
+    genre: "War"
+  },
+  {
+    title: "My Neighbor Totoro",
+    genre: "Children's"
+  },
+  {
+    title: "Kiki's Delivery Service",
+    genre: "Children's"
+  },
+  {
+    title: "Ponyo",
+    genre: "Children's"
+  }
+];
+
+//create server
+http.createServer((request, response) => {
+  let addr = request.url;
+  q = url.parse(addr, true);
+  filePath = "";
+});
+
+//setup the logger
+app.use(morgan("common"));
+
+//static files
+app.use(express.static("public"));
+
+//request routing
+app.get("/", (req, res) => {
+  res.send("Welcome to my app!");
+});
+
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
+//error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+//listen
+app.listen(8080, () => {
+  console.log("Your app is running on port 8080.");
+});
