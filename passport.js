bceeaa98b40563f6514e9b8e5d9bbcf818f80c9d
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-09-08 17:25:36 -0700

    add validate password to LocalStrategy

--- passport.js
+++ passport.js
@@ -23,6 +23,10 @@ passport.use(
                         message: "Incorrect username or password.",
                     });
                 }
+            if (!user.validatePassword(Password)) {
+                console.log("incorrect password");
+                return callback(null, false, { message: "Incorrect password."});
+            }
                 console.log("finished");
                 return callback(null, user);
             })
