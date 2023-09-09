3a372251564d38ff6efe7ded074cc979e053fc2d
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-09-09 14:39:23 -0700

    update connection to MongoDB database for Heroku

--- index.js
+++ index.js
@@ -44,8 +44,9 @@ require("./passport");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-//connect to MongoDB database movies from task 2.7
-mongoose.connect("mongodb://127.0.0.1:27017/test", {useNewUrlParser: true, useUnifiedTopology: true});
+//connect to MongoDB database
+// mongoose.connect("mongodb://127.0.0.1:27017/test", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //create server
 http.createServer((request, response) => {
