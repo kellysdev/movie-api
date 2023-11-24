eb00d96eef51af5de08cb2dcb36d8e6a82e583c8
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-11-24 09:37:09 -0800

    Fix indexOf capitalization

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ let allowedOrigins = ["http://localhost:8080/", "http://localhost:1234/",
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback (null, true);
-    if(allowedOrigins.indexof(origin) === -1) {
+    if(allowedOrigins.indexOf(origin) === -1) {
       let message = "The CORS policy for this application doesn't allow access from origin " + origin;
       return callback(new Error(message ), false);
     }
